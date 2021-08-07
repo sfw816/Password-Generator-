beca0f6fdcc7cf1d29b38e2a86ea69fd9d51743b
sfw816 <sfwalker12@gmail.com>
2021-08-07 12:49:28 -0500

    password length, complexity, possible values for loop

---
+++ script.js
@@ -0,0 +1,17 @@
+//generate random password
+function generate(){
+
+    //set password length and complexity
+    let complexity = document.getElementById("slider").value;
+
+    //possible password values
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+
+    let password = "";
+
+    //create for loop to choose password characters
+    for(var i = 0; i <= complexity; i++){
+        password = password.values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+    }
+}
+ 
