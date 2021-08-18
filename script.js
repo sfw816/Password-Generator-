b318e9ba240ffa9c1fed7303bf9d0c4ec532c88d
sfw816 <sfwalker12@gmail.com>
2021-08-18 18:22:10 -0500

    add password to textbox

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function generate(){
     for(var i = 0; i <= complexity; i++){
         password = password.values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
     }
-   
+   //add password to textbox/ display area
+   document.getElementById("display").value = password;
 }
  
